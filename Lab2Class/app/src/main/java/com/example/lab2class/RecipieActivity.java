433e3aa1b67fccfdba24db93bd2bb77b5a8773d7
Thiago Lyra Ganem <tlyraganem@gmail.com>
2023-09-17 12:34:43 -0700

    API Testing

--- Lab2Class/app/src/main/java/com/example/lab2class/RecipieActivity.java
+++ Lab2Class/app/src/main/java/com/example/lab2class/RecipieActivity.java
@@ -2,7 +2,10 @@ package com.example.lab2class;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecipieActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class RecipieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipie);
+
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener((View view)-> {
+            startActivity(new Intent(RecipieActivity.this, MainActivity.class));
+        });
+
+
+
     }
 }
