9f0954dba66f76f84576c0e7e32d541c2da9581c
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2023-10-31 11:39:24 -0700

    adding on click interface into demo

--- Lec7_classDemo/app/src/main/java/com/example/lec7demo/RecyclerViewActivity.java
+++ Lec7_classDemo/app/src/main/java/com/example/lec7demo/RecyclerViewActivity.java
@@ -2,27 +2,66 @@ package com.example.lec7demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerViewActivity extends AppCompatActivity {
+public class RecyclerViewActivity extends AppCompatActivity implements imgRecyclerAdapter.OnItemClickListener{
     List<GalleryImg> imgList = new ArrayList<>();
+    int id;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recicler_view);
+
+        Button btnShowImage = findViewById(R.id.btnShowImg);
+        img = findViewById(R.id.imageViewLarge2);
+
         addData();
-        imgRecyclerAdapter adpt = new imgRecyclerAdapter(imgList);
+        //imgRecyclerAdapter adpt = new imgRecyclerAdapter(imgList);
+//        imgRecyclerAdapter adpt = new imgRecyclerAdapter(imgList, new imgRecyclerAdapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(int i) {
+//                if(i!=-1) {
+//                    img.setImageResource(imgList.get(i).getImgPic());
+//                }
+//
+//            }
+//        });
+        imgRecyclerAdapter adpt = new imgRecyclerAdapter(imgList,this);
+
         RecyclerView recyclerView = findViewById(R.id.reciclerViewImages);
+
+        LinearLayoutManager lm = new LinearLayoutManager(this);
 
 
         GridLayoutManager gm = new GridLayoutManager(this,2);
-        recyclerView.setAdapter(adpt);
-        recyclerView.setLayoutManager(gm);
+        recyclerView.setAdapter(adpt);recyclerView.setLayoutManager(gm);
+        //recyclerView.setLayoutManager(lm);
+
+
+
+
+
+        btnShowImage.setOnClickListener((View view)  -> {
+            id  = adpt.getSelectedInd();
+            if(id!=-1) {
+                img.setImageResource(imgList.get(id).getImgPic());
+            }
+
+
+        });
+
+
 
 
     }
@@ -34,5 +73,12 @@ public class RecyclerViewActivity extends AppCompatActivity {
         imgList.add(new GalleryImg(105,"Panther",R.drawable.panther));
         imgList.add(new GalleryImg(106,"Polar Bear",R.drawable.polar));
 
+    }
+
+    @Override
+    public void onItemClick(int i) {
+        if(i!=-1) {
+                   img.setImageResource(imgList.get(i).getImgPic());
+              }
     }
 }
--- Lec7_classDemo/app/src/main/java/com/example/lec7demo/imgRecyclerAdapter.java
+++ Lec7_classDemo/app/src/main/java/com/example/lec7demo/imgRecyclerAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,9 +17,34 @@ import java.util.List;
 
 public class imgRecyclerAdapter extends RecyclerView.Adapter<imgRecyclerAdapter.ImageViewHolder> {
     List<GalleryImg> adapterImg;
+    int SelectedInd;
+    OnItemClickListener onItemClickListener;
+
+    public imgRecyclerAdapter(List<GalleryImg> adapterImg, OnItemClickListener onItemCLickListener) {
+        this.adapterImg = adapterImg;
+        this.onItemClickListener = onItemCLickListener;
+    }
 
     public imgRecyclerAdapter(List<GalleryImg> adapterImg) {
+
         this.adapterImg = adapterImg;
+        this.SelectedInd = -1;
+    }
+
+    public List<GalleryImg> getAdapterImg() {
+        return adapterImg;
+    }
+
+    public void setAdapterImg(List<GalleryImg> adapterImg) {
+        this.adapterImg = adapterImg;
+    }
+
+    public int getSelectedInd() {
+        return SelectedInd;
+    }
+
+    public void setSelectedInd(int selectedInd) {
+        SelectedInd = selectedInd;
     }
 
     @NonNull
@@ -35,7 +61,13 @@ public class imgRecyclerAdapter extends RecyclerView.Adapter<imgRecyclerAdapter.
         holder.imgViewItem.setImageResource(adapterImg.get(position).getImgPic());
         holder.txtViewItem.setText(adapterImg.get(position).getImgName());
         holder.txtViewItem.setGravity(Gravity.CENTER);
-        holder.itemView.setBackgroundColor(Color.parseColor("#f1a345"));
+        holder.itemView.setBackgroundColor(Color.parseColor("#FAFAFA"));
+
+        if(position==SelectedInd) {
+            holder.itemView.setBackgroundColor(Color.LTGRAY);
+        } else {
+            holder.itemView.setBackgroundColor((Color.parseColor("#FAFAFA")));
+        }
     }
 
     @Override
@@ -51,7 +83,22 @@ public class imgRecyclerAdapter extends RecyclerView.Adapter<imgRecyclerAdapter.
             super(itemView);
             imgViewItem = itemView.findViewById(R.id.imgViewExtItem);
             txtViewItem = itemView.findViewById(R.id.txtViewExtItem);
+
+            imgViewItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onItemClickListener.onItemClick(getAdapterPosition());
+                    //itemView.setBackgroundColor(Color.LTGRAY);
+                    SelectedInd = getAdapterPosition(); // if notifyDataChanged is called getAdapterPosition change to -1
+                    //Toast.makeText(view.getContext(), "ID:" + SelectedInd, Toast.LENGTH_SHORT).show();
+                    notifyDataSetChanged();
+                }
+            });
+
+
         }
     }
-
+    public interface  OnItemClickListener{
+        public void onItemClick(int i);
+    }
 }
