bd49f828f223b30b7aedf44ff8d18379fcabfb12
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2023-11-28 11:00:36 -0800

    Finishing class

--- Lec11_21Nov_LabFiles/DateDemo/app/src/main/java/com/example/datedemo/DogAdapter.java
+++ Lec11_21Nov_LabFiles/DateDemo/app/src/main/java/com/example/datedemo/DogAdapter.java
@@ -1,19 +1,27 @@
 package com.example.datedemo;
 
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.datedemo.databinding.LayoutDogitemBinding;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogViewHolder> {
     List<Dog> AdapterDogList;
+    OnItemClickListener onItemClickListener;
 
+    public DogAdapter(List<Dog> adapterDogList, OnItemClickListener onItemClickListener) {
+        AdapterDogList = adapterDogList;
+        this.onItemClickListener = onItemClickListener;
+    }
 
     public DogAdapter(List<Dog> adapterDogList) {
         AdapterDogList = adapterDogList;
@@ -34,12 +42,17 @@ public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogViewHolder> {
         return holder;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull DogViewHolder holder, int position) {
         holder.binding.txtViewId.setText(AdapterDogList.get(position).getId()+ " ");
         holder.binding.txtViewName.setText(AdapterDogList.get(position).getDogName());
         holder.binding.txtViewBreed.setText(AdapterDogList.get(position).getDogBreed());
         holder.binding.imgViewDogPic.setImageResource(AdapterDogList.get(position).getDogPicDrawable());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
+        String dobStr = formatter.format(AdapterDogList.get(position).getDogDob());
+        holder.binding.txtViewDOB.setText(dobStr);
     }
 
     @Override
@@ -56,7 +69,18 @@ public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogViewHolder> {
         public DogViewHolder(@NonNull View itemView , LayoutDogitemBinding binding) {
             super(itemView);
             this.binding = binding;
+            this.binding.getRoot().setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    onItemClickListener.onItemClick(getAdapterPosition());
+                }
+            });
         }
+    }
+    public interface OnItemClickListener{
+        public void onItemClick(int i);
+
     }
 
 }
--- Lec11_21Nov_LabFiles/DateDemo/app/src/main/java/com/example/datedemo/MainActivity.java
+++ Lec11_21Nov_LabFiles/DateDemo/app/src/main/java/com/example/datedemo/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.datedemo;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.example.datedemo.databinding.ActivityMainBinding;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -24,18 +27,37 @@ public class MainActivity extends AppCompatActivity {
     /// Database
     List<Dog> DogList = new ArrayList<>();
     String TAG = "DOGDemo";
-
+    ActivityMainBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ReadDogData();
+
+        setAdapterWithClick();
+        //setAdapterInfo();
+
         Log.d(TAG, "onCreate: Testing DogData" + DogList.get(0));
+
     }
-
-
+    public  void setAdapterWithClick() {
+        DogAdapter dogAdapter = new DogAdapter(DogList, new DogAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int i) {
+                binding.txtViewAdoptionSumary.setText("Thank you for taking" + DogList.get(i).getDogName() + "To their forever home");
+            }
+        });
+        binding.recyclerViewDogItems.setAdapter(dogAdapter);
+        binding.recyclerViewDogItems.setLayoutManager(new LinearLayoutManager(this));
+    }
+    public void setAdapterInfo() {
+        DogAdapter dogAdapter = new DogAdapter(DogList);
+        binding.recyclerViewDogItems.setAdapter(dogAdapter);
+        binding.recyclerViewDogItems.setLayoutManager(new LinearLayoutManager(this));
+    }
     public void ReadDogData() {
         InputStream inputStream = getResources().openRawResource(R.raw.doginfo);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -59,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 //yyyy- 4 digit year (e.g., 01,11,10,12)
 
                 //dd - two digit dates(e.g., 01,08,31,18)
-                //mm - 2 digit number
+                //MM - 2 digit number
                 //yy two digit year
 
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yyyy");
@@ -68,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
 
                 Dog eachDog = new Dog(dogId,dogName,dogBreed,dogDrawable,dob);
                 DogList.add(eachDog);
+
+
 
             }
 
